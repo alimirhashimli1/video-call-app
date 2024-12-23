d3230783805d8991569d3d1816470dea1ca9508f
alimirhashimli1 <alimirhashimli@gmail.com>
2024-12-24 00:19:10 +0100

    changed ts config

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var config = {
+    content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
+exports.default = config;
