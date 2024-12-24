7769adf7d3106eb83ec965f1ccf4d2d041f1eeea
alimirhashimli1 <alimirhashimli@gmail.com>
2024-12-24 03:19:41 +0100

    added backend url

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { useNavigate, BrowserRouter as Router, Routes, Route, useParams } from 'react-router-dom';
+import { useNavigate, BrowserRouter as  Routes, Route, useParams } from 'react-router-dom';
 import io, { Socket } from 'socket.io-client';
 import 'tailwindcss/tailwind.css';
 
@@ -8,12 +8,10 @@ const socket: Socket = io('https://video-call-app-1-o75x.onrender.com');
 
 const App: React.FC = () => {
   return (
-    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/room/:roomId" element={<Room />} />
       </Routes>
-    </Router>
   );
 };
 
